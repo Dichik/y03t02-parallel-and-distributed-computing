47901e94a5467f7252bea0fece496f33ec5eeb7c
Dichik <omeluan.dima@gmail.com>
2023-03-10 14:32:51 +0200

    bug: add Kahan algorithm

--- src/main/java/org/example/strategies/MultiplyOperation.java
+++ src/main/java/org/example/strategies/MultiplyOperation.java
@@ -4,9 +4,15 @@ public class MultiplyOperation implements CalculationStrategy {
     @Override
     public void calculate(double[][] A, double[][] B, double[][] result, int row) {
         for (int j = 0; j < B[0].length; j++) {
+            double sum = 0.0;
+            double error = 0.0;
             for (int k = 0; k < A[0].length; k++) {
-                result[row][j] += A[row][k] * B[k][j];
+                double x = A[row][k] * B[k][j] - error;
+                double tempSum = sum + x;
+                error = (tempSum - sum) - x;
+                sum = tempSum;
             }
+            result[row][j] = sum;
         }
     }
 
